c72e2ce387498bc3cd250c53cf7a5d14e8baa5ea
vishangi <vishangid@gmail.com>
2022-06-20 23:27:42 +0530

    Added : Adding element to stack list

---
+++ StackAndQueue.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+public class StackAndQueue {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Stack And Queue List  Program");
+        Stack stack = new Stack();
+        stack.insert(70);
+        stack.insert(30);
+        stack.insert(56);
+        stack.toPrint();
+
+	}
+
+}
