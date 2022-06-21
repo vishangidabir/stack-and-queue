4b04ba06218ad83a03ebaaa5e88ad595010e2467
vishangi <vishangid@gmail.com>
2022-06-21 18:59:52 +0530

    Added : Adding queue element in the list

---
+++ Queue.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+import java.util.LinkedList;
+public class Queue {
+	LinkedList<Integer> queue = new LinkedList<>();
+
+	//Method to add element
+    public void enQueue(int data) {
+        queue.add(data);
+    }
+
+    //Method to print the element
+    public void toPrint() {
+        System.out.println("Queue list is :");
+        for (Integer i : queue) {
+            System.out.println(" " + i);
+        }
+    }
+}
