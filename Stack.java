6f196616a2ae70b7cb6309e53fa0e0fec7384589
vishangi <vishangid@gmail.com>
2022-06-21 18:44:28 +0530

    Added : Using peek abd pop until list is empty

--- Stack.java
+++ Stack.java
@@ -3,17 +3,32 @@ package com.bridgelabz;
 import java.util.LinkedList;
 
 public class Stack {
-	LinkedList<Integer> linkedList = new LinkedList<>();
+	LinkedList<Integer> stack = new LinkedList<>();
+	
+	//Method to insert the element 
+    public void insert(int data) {
+        stack.addFirst(data);
+    }
 
-	//Method to insert element in stack
-	public void insert(int data) {
-		linkedList.addFirst(data);
-	}
+    //Method to present the top element 
+    public void peek() {
+        System.out.println("stack list in the peek is :" + stack.peek());
+    }
 
-	//Method to print the stack
-	public void toPrint() {
-		for (Integer integer : linkedList) {
-			System.out.println(" " + integer);
-		}
-	}
+    //Method to remove the element 
+    public void pop() {
+        if (stack.size() == 0) {
+            System.out.println("List is empty");
+        } else {
+            stack.pop();
+        }
+    }
+
+    //Method to display the element 
+    public void toPrint() {
+        System.out.println("Stack list is :");
+        for (Integer i : stack) {
+            System.out.println(" " + i);
+        }
+    }
 }
