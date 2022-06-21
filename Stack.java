7ddc2947aeb7a627e68a1186354840ea95a06baa
vishangi <vishangid@gmail.com>
2022-06-21 18:30:36 +0530

    modified : Adding element to the stack

--- Stack.java
+++ Stack.java
@@ -5,10 +5,12 @@ import java.util.LinkedList;
 public class Stack {
 	LinkedList<Integer> linkedList = new LinkedList<>();
 
+	//Method to insert element in stack
 	public void insert(int data) {
 		linkedList.addFirst(data);
 	}
 
+	//Method to print the stack
 	public void toPrint() {
 		for (Integer integer : linkedList) {
 			System.out.println(" " + integer);
