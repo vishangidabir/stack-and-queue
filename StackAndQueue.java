709226c7e3035071f9188414d6656546f7fd9fa8
vishangi <vishangid@gmail.com>
2022-06-21 18:25:01 +0530

    Added : Adding element to the stack

--- StackAndQueue.java
+++ StackAndQueue.java
@@ -4,11 +4,11 @@ public class StackAndQueue {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Stack And Queue List  Program");
-        Stack stack = new Stack();
-        stack.insert(70);
-        stack.insert(30);
-        stack.insert(56);
-        stack.toPrint();
+		Stack stack = new Stack();
+		stack.insert(70);
+		stack.insert(30);
+		stack.insert(56);
+		stack.toPrint();
 
 	}
 
