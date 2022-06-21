044c50ca322dec256a5a3bfe67d03c8ccda46b5e
vishangi <vishangid@gmail.com>
2022-06-21 19:06:10 +0530

    Added : Adding dequeue in the list is empty

--- StackAndQueue1.java
+++ StackAndQueue1.java
@@ -2,11 +2,17 @@ package com.bridgelabz;
 
 public class StackAndQueue1 {
 	public static void main(String[] args) {
-        System.out.println("Welcome to Stack And Queue List  Program");
-        Queue queue = new Queue();
-        queue.enQueue(56);
-        queue.enQueue(30);
-        queue.enQueue(70);
-        queue.toPrint();
-}
+		System.out.println("Welcome to Stack And Queue List  Program");
+		Queue queue = new Queue();
+		queue.enQueue(56);
+		queue.enQueue(30);
+		queue.enQueue(70);
+		queue.toPrint();
+		queue.deQueue();
+		queue.toPrint();
+		queue.deQueue();
+		queue.toPrint();
+		queue.deQueue();
+		queue.toPrint();
+	}
 }
