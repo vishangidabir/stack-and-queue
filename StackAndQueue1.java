93cb7753750609878eeb9418f6a6657bfbd915c6
vishangi <vishangid@gmail.com>
2022-06-21 18:58:50 +0530

    Added : Adding queue element in the list

---
+++ StackAndQueue1.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class StackAndQueue1 {
+	public static void main(String[] args) {
+        System.out.println("Welcome to Stack And Queue List  Program");
+        Queue queue = new Queue();
+        queue.enQueue(56);
+        queue.enQueue(30);
+        queue.enQueue(70);
+        queue.toPrint();
+}
+}
